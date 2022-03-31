3b84ee249c72df907e85dbf87f4cc3a5fbe82c4e
vctrblck <sphesihlentshingila@protonmail.com>
2022-03-31 17:28:35 +0200

    Added error handler

--- server/express.js
+++ server/express.js
@@ -1,13 +1,20 @@
-// express.js
+// express.js -- Express application entry point
 
 // Code:
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const parseError = require('./utility/error');
+const shopperRoutes = require('./route/shopper');
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/api', shopperRoutes);
+
+app.use(parseError);
 
 module.exports = app;
 
