f7888ed1eb17c9d389a8ca87f0c0290d31dbcd32
vctrblck <sphesihlentshingila@protonmail.com>
2022-03-31 14:24:13 +0200

    Fixed database connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@
 
 // Code:
 
+const mongoose = require('mongoose');
+
 const config = require('./../config/config');
 const app = require('./express');
+
+// Connect database
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoURI);
+
+mongoose.connection.once('open', function() {
+    console.log('Database connection successful...')
+}).on('error', () => {
+    console.log(`Database connection failure on ${config.mongoUri}...`);
+})
 
 // Start server
 app.listen(config.port, (error) => {
