48581d093154efaed74637bcd6e49b30f117139b
vctrblck <sphesihlentshingila@protonmail.com>
2022-03-31 22:34:10 +0200

    Added static file functionality to express

--- server/express.js
+++ server/express.js
@@ -6,12 +6,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const parseError = require('./utility/error');
+const homeRoutes = require('./route/home');
 const shopperRoutes = require('./route/shopper');
 
 const app = express();
 
+app.use(express.static('dist'));
 app.use(bodyParser.json());
 
+app.use(homeRoutes);
 app.use('/api', shopperRoutes);
 
 app.use(parseError);
