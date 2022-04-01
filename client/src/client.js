747445712f647bdc33dfdaeedbf0201aa47f0ae3
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-01 07:33:02 +0200

    Added front end boilerplate

---
+++ client/src/client.js
@@ -0,0 +1,12 @@
+// client.js --- Client entry point
+
+// Code:
+
+const react = require('react');
+const reactDOM = require('react-dom');
+
+const App = require('./app.js');
+
+reactDOM.render(<App/>, document.getElementById('root'));
+
+// client.js ends here
