1b69057c9e37b1b9ff82c5d6aa62e8be147779f0
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-06 12:23:12 +0200

    Adjusted database settings for easy development

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const config = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 4000,
     MONGODB_URI: process.env.MONGODB_URI
-        || 'mongodb+srv://duty:duty@cluster0.5ts0l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        //|| 'mongodb+srv://duty:duty@cluster0.5ts0l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
         || 'mongodb://localhost:27017/grocery-shopping-list'
 }
 
