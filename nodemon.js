91cf85b94fb67c8956fccf10c8f5dcf995cd7175
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-22 13:42:04 +0200

    Drafting

--- nodemon.js
+++
@@ -1,9 +0,0 @@
-// nodemon.js --- Nodemon configuration
-
-{
-    "verbose": false,
-    "watch": [ "./server" ],
-    "exec": "./node_modules/.bin/webpack --mode=development --config webpack.config.server.js && node ./dist/server.bundle.js"
-}
-
-// nodemon.js ends here
